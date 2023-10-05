ce75e30f3b30ba6e4d8fe28382597acaf515cb70
rachan93 <anas.rachafi_el_hausi@ifosup.wavre.be>
2023-10-05 19:27:27 +0200

    médianes et tests correspondants

--- tests/Functions/AverageTest.php
+++ tests/Functions/AverageTest.php
@@ -10,10 +10,21 @@ test('moyenne', function () {
 });
 
 
-test('moyenne sans input', function () {
+test('moyenne avec input vide', function () {
     $list = [];
 
     expect(function() use ($list) {
         Average::calculate($list);
     })->toThrow(Exception::class);
+});
+
+/*test('moyenne avec input égal à 0', function () {
+    $list = [0];
+
+    expect(Average::calculate($list))->toBe(0.0);
+});*/
+test('moyenne avec input mixte', function () {
+    $list = ['a', [1,2,3], 15];
+
+    expect(Average::calculate($list))->toBe(5.0);
 });
